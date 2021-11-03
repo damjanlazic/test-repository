01c462fe3460f99d66e2a5d2708e4a72af23f192
damjan <damjan.lazic@gmail.com>
2021-11-03 20:44:52 +0100

    final changes

--- app.py
+++ app.py
@@ -5,4 +5,13 @@ reverse_name=""
 for ch in range(len(name)-1, -1, -1):
     reverse_name += name[ch]
     print(name[ch])
+reverse_name = reverse_name.lower()
+reverse_name = reverse_name.capitalize()
 print("From now on, you are to be called:", reverse_name)
+satisfied = input("Satisfied? (y,n)")
+if satisfied == "y" :
+    print("Goodbye {}, have a great life!".format(reverse_name))
+else:
+    new_name = "Đoka"
+    print(f"Best we call you {new_name} then!")
+    print("Goodbye {}, have a great life!".format(new_name))
